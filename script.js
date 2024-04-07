3518b320f04119899c3c276cc00a3b2576970f96
Dipto Neogi <81651700+Dipto07@users.noreply.github.com>
2024-04-08 01:23:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,10 +4,10 @@ function isSameType(value1, value2) {
     if (isNaN(value1) && isNaN(value2)) {
         return true;
     }
-		else if (typeof value1==typeof value2) {
+		if (typeof value1==typeof value2) {
 			return true;
 		}
-	else  (typeof value1 === typeof value2) {
+	  if (typeof value1 === typeof value2) {
 		return false;
 	}
 }
